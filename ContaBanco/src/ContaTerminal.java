4ee2edca830f18a13b5acef1bb5e76ee1203971f
Matheus Lima <matheuslimacarneirors@gmail.com>
2024-08-21 17:04:07 -0300

    feito desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+
+        int numero = 0;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+
+        System.out.println("Por favor digite seu nome: ");
+        nomeCliente = sc.nextLine();
+
+
+        System.out.println("Por favor digite o numero do Usuario: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor digite sua agência: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor digite seu saldo: ");
+        saldo = sc.nextDouble();
+
+        if(saldo < 0){
+            System.out.println("Saldo invalido para saque.");
+            System.out.println("Por favor digite um saldo valido: ");
+            saldo = sc.nextDouble();
+        }
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        ", conta "+ numero + " e seu saldo "+ saldo + " já está disponível para saque.");
     }
 }
